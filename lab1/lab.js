324e2aa51f1249f888ae1f34f4c196020cbd8ea1
Natanbratan <nnatan930@gmail.com>
2020-09-14 14:40:40 +0300

    laba1

--- lab1/lab.js
+++ lab1/lab.js
@@ -7,7 +7,7 @@ function task1(n) {
 }
 
 function task2(a, b, c) {
-    if (a % b % c == 0) 
+    if (a % b && a % c == 0) 
         console.log('число ' + a + ' нацело делится на ' + b + ' и на '  + c)
     else console.log ('Первое число не делится на второе и третье нацело')
 }
@@ -28,14 +28,14 @@ function capitalizeIt(str) {
 task1(6)
 task2(6, 4, 5)
 task2(14, 2, 7)
-console.log(capitalizeIt("help me pls"))
+console.log(capitalizeIt("privet"))
 
 
 
 /*Создать html-страницу с инпутом и кнопкой.
  По нажатию на кнопку выводить текст из инпута в блок div ниже на странице.*/
-const m = document.getElementById("228")
-const button = document.getElementById("1337")
+const m = document.getElementById("stroka")
+const button = document.getElementById("knopka")
 button.addEventListener("click",()=>{
-    document.getElementById("1488").innerText = m.value
+    document.getElementById("blok").innerText = m.value
 })
